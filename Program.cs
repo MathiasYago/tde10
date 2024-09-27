be8d412a025aec38362e26f23b384662203ad45f
mathiasyago <mathias.santos@rede.ulbra.br>
2024-09-26 21:44:06 -0300

    teste

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Retangulo r1 = new Retangulo();
+Retangulo r2 = new Retangulo(5);
+Retangulo r3 = new Retangulo(4, 6);
+
+Console.WriteLine($"A area do retangulo R1 = {r1.CalcularArea()}");
+Console.WriteLine($"O perimetro do retangulo  R1 = {r1.CalcularAreaPerimetro()}");
+
+Console.WriteLine($"A area do retangulo R2 = {r2.CalcularArea()}");
+Console.WriteLine($"O perimetro do retangulo  R2 = {r2.CalcularAreaPerimetro()}");
+
+Console.WriteLine($"A area do retangulo R3 = {r3.CalcularArea()}");
+Console.WriteLine($"O perimetro do retangulo  R3 = {r3.CalcularAreaPerimetro()}");
+
+r3.Redimensionar(2);
+Console.WriteLine($"A area do retangulo R2 = {r2.CalcularArea()}");
+Console.WriteLine($"O perimetro do retangulo  R2 = {r2.CalcularAreaPerimetro()}");
+
+r2.Redimensionar(10,15);
+Console.WriteLine($"A area do retangulo R3 = {r3.CalcularArea()}");
+Console.WriteLine($"O perimetro do retangulo  R3 = {r3.CalcularAreaPerimetro()}");
+
+
---
+++ Retangulo.cs
@@ -0,0 +1,48 @@
+
+public class Retangulo
+{
+    public double altura;
+    
+    public double largura;
+
+public Retangulo()
+{
+    altura= 0;
+    largura= 0;
+}
+public Retangulo(double valor)
+{
+    altura = valor;
+    largura = valor;
+}
+
+public Retangulo (double altura, double largura)
+{
+    this.altura = altura;
+    this.largura = largura;
+}
+public double CalcularArea()
+{
+    return altura * largura;
+}
+public double CalcularAreaPerimetro()
+{
+    return largura + altura + largura + altura;
+}
+public void Redimensionar()
+{
+
+
+}
+
+public void Redimensionar( double fator)
+{
+    altura *= fator;
+    largura *= fator;
+}
+public void Redimensionar(double fator, double altura)
+{
+    this.altura= altura;
+    this.largura = largura;
+}
+}
